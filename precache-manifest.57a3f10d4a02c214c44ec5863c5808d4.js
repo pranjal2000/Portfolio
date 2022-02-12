44823be6dbab4c89cdcc99bfd655dca5b848bfc4
Pranjal Singh <pranjalsingh@probo.in>
2022-02-12 16:32:59 +0530

    first commit

---
+++ precache-manifest.57a3f10d4a02c214c44ec5863c5808d4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b111182494341bf9bfe29088f4903c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1e8717b3fc70e311a54",
+    "url": "/static/css/main.2d2c2cbb.chunk.css"
+  },
+  {
+    "revision": "12274d6e8d34d5beec43",
+    "url": "/static/js/2.91ffcbc9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.91ffcbc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e8717b3fc70e311a54",
+    "url": "/static/js/main.1c9b15ce.chunk.js"
+  },
+  {
+    "revision": "fba0a7ae82e3114643a7",
+    "url": "/static/js/runtime-main.a0576299.js"
+  },
+  {
+    "revision": "fde3c0b1ed24a390fd6180981c7439c8",
+    "url": "/static/media/award.fde3c0b1.jpg"
+  },
+  {
+    "revision": "305b7b7bf17b8341d4c61d0dc017fb9d",
+    "url": "/static/media/cloud.305b7b7b.svg"
+  },
+  {
+    "revision": "f9c8b12757db632cbd634271b1cd5b10",
+    "url": "/static/media/data_analytics_.f9c8b127.svg"
+  },
+  {
+    "revision": "8c7ae19756b45657b73055a1142be613",
+    "url": "/static/media/data_process.8c7ae197.svg"
+  },
+  {
+    "revision": "73e3ab59751b8dd4bf5c6c8be3335e21",
+    "url": "/static/media/language.73e3ab59.png"
+  },
+  {
+    "revision": "54c3adc538f06c7819b16b934f876911",
+    "url": "/static/media/mountain.54c3adc5.svg"
+  },
+  {
+    "revision": "921df655e6f1ce400074caecfdf30605",
+    "url": "/static/media/process_building.921df655.svg"
+  },
+  {
+    "revision": "9e5663bc5a0db818f2eaf47a2567f092",
+    "url": "/static/media/publish.9e5663bc.jpg"
+  },
+  {
+    "revision": "756b31836ca3dfc446be2b22b4962be9",
+    "url": "/static/media/skill.756b3183.jpg"
+  },
+  {
+    "revision": "fafe0d09bc8d6a2b0848c96d52b68f36",
+    "url": "/static/media/sun.fafe0d09.svg"
+  }
+]);
